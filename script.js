ea34326864da9cf0c5deac2b6c631a35d00bf6b5
oliullah05 <mdolihasan@gmail.com>
2023-03-03 19:51:02 +0600

    show all button done

--- script.js
+++ script.js
@@ -1,18 +1,31 @@
 
 // all cards data fetch
-const allcardsData= async()=>{
+const allcardsData= async(max)=>{
     const url =`https://openapi.programming-hero.com/api/ai/tools`
     const res = await fetch(url);
     const datum =await res.json(res);
-    displayAllcardsData(datum.data.tools)
+    displayAllcardsData(datum.data.tools,max)
 
 }
 
 // all card data display
-const displayAllcardsData= (datum)=>{
+const displayAllcardsData= (datum,max)=>{
+ 
+ 
     const getCardsDiv = document.getElementById("cards");
+    getCardsDiv.innerHTML ="";
+    if(max===6){
+datum = datum;
+
+    }
+   else if(datum.length>6){
+      datum = datum.slice(0,6)
+      document.getElementById("show-all-button").classList.remove('d-none')
+    }
+    // console.log(datum)
+
     datum.forEach(element => {
-        console.log(element.id)
+        // console.log(element.id)
         const createDiv = document.createElement('div');
         createDiv.classList.add('col');
         createDiv.innerHTML=`
@@ -56,6 +69,7 @@ const displayAllcardsData= (datum)=>{
 // deatils icon single data fetch
 const singleDetailsdata =async(id)=>{
     const url =`https://openapi.programming-hero.com/api/ai/tool/${id}`
+    // console.log(url)
     const res = await fetch(url);
     const datum =await res.json(res);
     displaysingleDetailsdata(datum.data)
@@ -63,5 +77,80 @@ const singleDetailsdata =async(id)=>{
 
 // deatils icon single data display
 const displaysingleDetailsdata=(data)=>{
-console.log(data)
+  const modalSection = document.getElementById('modal-body');
+modalSection.innerHTML+=`
+<div class="modal-header">
+<h5 class="modal-title" id="detailsModalLabel">${data.tool_name}</h5>
+<button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+</div>
+
+<div id="modal-body" class="modal-body row row-cols-1 row-cols-md-2 g-4">
+
+<div class="col-6">
+<h6 id="modal-intro" ></h6>
+
+<div class="d-flex  align-items-center gap-2 fw-semibold text-center p-2">
+<div class="bg-light rounded-2 text-success p-4"><span>$10/<br>month<br>Basic</span></div>
+<div  class="bg-light rounded-2 text-warning p-4"><span>$10/<br>month<br>Basic</span></div>
+<div class="bg-light rounded-2 text-danger p-4"><span>$10/<br>month<br>Basic</span></div>
+</div>
+
+
+<div class="d-flex gap-2">
+<div>
+<h4>Features</h4>
+<ul>
+<li id="features1" ></li>
+<li id="features2" >Multilingual support</li>
+<li id="features3" >Seamless integration</li>
+</ul>
+</div>
+
+<div>
+<h4>Integrations</h4>
+<ul>
+<li>FB Messenger</li>
+<li>Slack</li>
+<li>Telegram</li>
+</ul>
+</div>
+</div>
+
+
+
+</div>
+<div class="col-6">
+<img src="" alt="" srcset="">
+<p id="accuracy" class="p-2 text-white bg-danger w-50 border border-3">94% accuracy</p>
+<h3>Hi, how are you doing today?</h3>
+<p>I'm doing well, thank you for asking. How can I assist you today?</p>
+</div>
+
+</div>
+<div class="modal-footer">
+<button type="button" class="btn btn-secondary" data-bs-dismiss="modal">Close</button>
+</div>
+
+`
+
+
+
+
+// const modalTitle = document.getElementById('detailsModalLabel') ;
+// modalTitle.innerText= `${data.tool_name}`;
+// const modalIntro = document.getElementById('modal-intro');
+// modalIntro.innerText=`${data.description}`;
+// const features1 = document.getElementById('features1');
+// features1.innerText=`${data.features.feature_name}`
+
+
+
+  
+// console.log(data.tool_name)
+}
+
+function showAll(max){
+document.getElementById("show-all-button").classList.add('d-none');
+const showAllbutton = document.getElementById('show-all-button');
+allcardsData(max)
 }
