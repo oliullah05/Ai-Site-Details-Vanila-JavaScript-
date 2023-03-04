1883cbc178aabbfca9a747f866cf18feacb2dbec
oliullah05 <mdolihasan@gmail.com>
2023-03-04 22:00:14 +0600

    all task done

--- script.js
+++ script.js
@@ -1,4 +1,6 @@
 
+// all cards api
+
 const apiDataLoad = dataLimit => {
     const url = 'https://openapi.programming-hero.com/api/ai/tools'
     fetch(url)
@@ -11,13 +13,13 @@ const apiDataLoad = dataLimit => {
 const displayApiDataLoad = (data, dataLimit) => {
     Spinner(true)
 
-    const showMoreContainer = document.getElementById('show-more')
+    const showMoreSection = document.getElementById('show-more')
     if (dataLimit !== 6 && data.length > 6) {
         data = data.slice(0, 6)
-        showMoreContainer.classList.remove('d-none')
+        showMoreSection.classList.remove('d-none')
     }
     else {
-        showMoreContainer.classList.add('d-none')
+        showMoreSection.classList.add('d-none')
     }
 
     const cardsSection = document.getElementById('cards-container')
@@ -26,21 +28,26 @@ const displayApiDataLoad = (data, dataLimit) => {
 
         cardsSection.innerHTML += `
         <div class="col">
-        <div class="card h-100 p-2 shadow border-0">
-            <img src="${card.image}" class="card-img-top img-fluid img-thumbnail h-50" alt="">
+        <div class="card h-100 p-2 shadow">
+            <img src="${card.image}" class="card-img-top img-fluid h-100" alt="">
             <div class="card-body">
+
                 <h5 class="card-title">Features</h5>
-                <ol class="list-group p-3">${card.features.map(list => `<li>${list}</li>`).join('')}</ol>
+                <ol class="list-group p-3">${card.features.map(feature => `<li>${feature}</li>`).join('')}</ol>
+
                 <hr>
+
                 <div class="d-flex justify-content-between align-items-center">
                     <div>
                         <h5 class="card-title">${card.name}</h5>
-                        <div class="d-flex gap-2  align-items-center">
+                        <div class="d-flex gap-2 align-items-center">
                             <img src="Vector.png"</img>
                             <p class="card-text">${card.published_in}</p>
                         </div>
                     </div>
+
                     <div onclick ="apiDetailsDataLoad('${card.id}')" style="width: 50px;" data-bs-toggle="modal" data-bs-target="#detailsModal"><img src="deatils-icon.png"</img></div>
+
                 </div>
             </div>
         </div>
@@ -86,13 +93,13 @@ const displayApiDetailsDataLoad = (modalData) => {
     modalBody.innerHTML = `
     <div class="row row-cols-1 row-cols-md-2 g-4">
     <div class="col">
-        <div class="card bg-info border-0 shadow-lg bg-opacity-25 h-100">
-            <div class="card-body p-3">
+        <div class="card bg-danger bg-gradient shadow h-100">
+            <div class="card-body p-3 text-white">
                 <h5 class="card-title">${description ? description : 'HEllo bro'}</h5>
-                <div class="d-flex justify-content-around gap-2 fw-semibold text-center my-5">
-                    <div class="bg-light rounded p-lg-2"><span class="text-success">${pricing ? pricing[0].price : 'Free of Cost'} <br> ${pricing ? pricing[0].plan : 'Basic'}</span></div>
-                    <div class="bg-light rounded p-lg-2"><span class="text-warning">${pricing ? pricing[1].price : 'Free of Cost'} <br> ${pricing ? pricing[1].plan : 'Pro'}</span></div>
-                    <div class="bg-light rounded p-lg-2"><span class="text-danger">${pricing ? pricing[2].price : 'Free of Cost'} <br> ${pricing ? pricing[2].plan : 'Enterprise'}</span></div>
+                <div class="d-flex justify-content-around text-black gap-2 fw-semibold text-center my-5">
+                    <div class="bg-light rounded p-lg-2"><span>${pricing ? pricing[0].price : 'Free of Cost'} <br> ${pricing ? pricing[0].plan : 'Basic'}</span></div>
+                    <div class="bg-light rounded p-lg-2"><span>${pricing ? pricing[1].price : 'Free of Cost'} <br> ${pricing ? pricing[1].plan : 'Pro'}</span></div>
+                    <div class="bg-light rounded p-lg-2"><span>${pricing ? pricing[2].price : 'Free of Cost'} <br> ${pricing ? pricing[2].plan : 'Enterprise'}</span></div>
                 </div>
 
                 <div class="d-flex justify-content-between">
@@ -109,7 +116,7 @@ const displayApiDetailsDataLoad = (modalData) => {
         </div>
     </div>
     <div class="col">
-        <div class="card text-center shadow-lg border-0 ">
+        <div class="card text-center shadow h-100 ">
             <div><span class="badge text-bg-danger w-30 p-2 position-absolute end-0">${accuracy.score ? accuracy.score * 100 + accuracyString : ''}</span>
             <img src="${image_link[0]}" class="card-img-top" alt=""></div>
             <div class="card-body my-5">
